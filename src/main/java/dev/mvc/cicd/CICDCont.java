067a0c6e0011800d21ccf0e287410a5900b1e96b
yoon969 <dbsrn1224@gmail.com>
2025-08-04 15:57:11 +0900

    CICD 2차 테스트

--- src/main/java/dev/mvc/cicd/CICDCont.java
+++ src/main/java/dev/mvc/cicd/CICDCont.java
@@ -10,30 +10,32 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 public class CICDCont {
   // http://localhost:9091/cicd?sw=-1
-  // http://localhost:9091/cicd?sw=0
   // http://localhost:9091/cicd?sw=1
+  // http://localhost:9091/cicd?sw=0
   // http://localhost:9091/cicd?sw=2
+  // http://localhost:9091/cicd?sw=3
+  // http://121.78.128.17:9091/cicd?sw=3
   @GetMapping("/cicd")
-  public String cicd(@RequestParam(name="sw", defaultValue = "1") Integer sw) {
+  public String cicd(@RequestParam(defaultValue = "1") Integer sw) {
     String msg = "";
     
     if (sw == -1) {
-      msg = "/cicd 호출됨, error 로그rm";
+      msg = "/cide 호출됨, error 로그";
       log.error(msg);
     } else if (sw == 0) {
-      msg = "/cicd 호출됨, warn 로그";
+      msg = "/cide 호출됨, warn 로그";
       log.warn(msg);
     } else if (sw == 1) {
       msg = "/cicd 호출됨, info 로그";
       log.info(msg);
     } else if (sw == 2) {
-      msg = "/cicd workflow 테스트";
+      msg = "/cide workflow 테스트";
       log.info(msg);
     } else if (sw == 3) {
-      msg = "/cicd workflow 테스트2";
+      msg = "/cicd Github action + gCloud 최종 테스트 1";
       log.info(msg);
     } else if (sw == 4) {
-      msg = "/cicd workflow 테스트3";
+      msg = "/cicd Github action + gCloud 최종 테스트 2";
       log.info(msg);
     }
     
